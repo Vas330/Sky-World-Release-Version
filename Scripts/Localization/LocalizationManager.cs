787d2ab2c4681b9d8bcd6262f24c4a0d1ff5c79b
Vas330 <85131915+Vas330@users.noreply.github.com>
2023-01-02 02:08:15 +0300

    Обновление 2
    Добавлено:(Горизонтальная синхронизация, локализация)

---
+++ Scripts/Localization/LocalizationManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalizationManager : MonoBehaviour
+{
+    public void Ru()
+    {
+        string language = "Ru";
+        PlayerPrefs.SetString("Language", language);
+    }
+
+    public void Eng()
+    {
+        string language = "Eng";
+        PlayerPrefs.SetString("Language", language);
+    }
+
+    public void Zh()
+    {
+        string language = "China";
+        PlayerPrefs.SetString("Language", language);
+    }
+}
---
+++ Scripts/Localization/TextLanguage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextLanguage : MonoBehaviour
+{
+    public string language;
+    Text text;
+
+    public string textRu;
+    public string textEng;
+    public string textZh;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        language = PlayerPrefs.GetString("Language");
+
+        if (language == "" || language == "Eng")
+        {
+            text.text = textEng;
+        }
+        else if (language == "Ru")
+        {
+            text.text = textRu;
+        }
+        else if (language == "China")
+        {
+            text.text = textZh;
+        }
+    }
+}
